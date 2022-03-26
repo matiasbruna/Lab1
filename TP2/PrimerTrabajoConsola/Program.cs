6c5eae0ac256e0a6a4f86f5515b955d71b23452a
Matias Bruna <matias.b.dev@gmail.com>
2022-03-25 23:24:24 -0300

    Cree el archivo de y el codigo del trabajo

---
+++ TP2/PrimerTrabajoConsola/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace PrimerTrabajoPractico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Escribe tu Apellido y Nombre");
+            var _nombre = Console.ReadLine();
+            
+            Console.WriteLine("Escribe tu Domicilio");
+            var _domicilio = Console.ReadLine();
+
+            Console.WriteLine("Escribe tu Telefono");
+            var _telefono = Console.ReadLine();
+
+            Console.WriteLine("Escribe tu Correo Electronico");
+            var _email = Console.ReadLine();
+
+            Console.WriteLine("Escribe tu Ciudad");
+            var _ciudad = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine(_nombre);
+            Console.WriteLine(_domicilio);
+            Console.WriteLine(_telefono);
+            Console.WriteLine(_email);
+            Console.WriteLine(_ciudad);
+
+
+
+        }
+    }
+}
