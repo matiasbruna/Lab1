71b3f07463774d8f0471ff63bf2d459723b07d80
Matias Bruna <matias.b.dev@gmail.com>
2022-03-29 19:17:46 -0300

    Cree la carpeta con el segundo proyecto

---
+++ TP2/PrimerProyectoLab/Program.cs
@@ -0,0 +1,9 @@
+﻿
+        string nombre;
+        int legajo;
+        int año ;
+        string carrera;            
+
+        Console.WriteLine("Ingrese su Apellido y Nombre");
+       
+        nombre = Console.ReadLine();
