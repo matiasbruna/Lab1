e9f4d47161114d7c18e03063ec8c80d99103b334
Matias Bruna <matias.b.dev@gmail.com>
2022-03-29 20:00:19 -0300

    cambie interior del proyecto

--- TP2/PrimerProyectoLab/Program.cs
+++ TP2/PrimerProyectoLab/Program.cs
@@ -1,9 +1,28 @@
-﻿
-        string nombre;
-        int legajo;
-        int año ;
-        string carrera;            
+﻿namespace PrimerTrabajoPractico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Escribe tu Apellido y Nombre");
+            var _nombre = Console.ReadLine();
+            
+            Console.WriteLine("Escribe tu Lejago de Estudiante");
+            var _domicilio = Console.ReadLine();
 
-        Console.WriteLine("Ingrese su Apellido y Nombre");
-       
-        nombre = Console.ReadLine();
+            Console.WriteLine("Año de Ingreso");
+            var _telefono = Console.ReadLine();
+
+            Console.WriteLine("Carrera que estudia");
+            var _email = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine(_nombre);
+            Console.WriteLine(_domicilio);
+            Console.WriteLine(_telefono);
+            Console.WriteLine(_email);
+        }
+    }
+}
