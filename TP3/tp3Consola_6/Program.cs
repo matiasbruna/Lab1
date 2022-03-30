c239eb1a2148f074ced2ec0602e271d33791d25d
Matias Bruna <matias.b.dev@gmail.com>
2022-03-30 15:18:21 -0300

    Agrege dos ejercicios mas a la carpeta de tp3, ademas resolvi el ejercio 7 de las struct y aprendi a declararlas y usarlas.

---
+++ TP3/tp3Consola_6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace tp3Consola_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Colores Pintura = Colores.Amarillo;
+            Console.WriteLine(Pintura);
+        }
+    }
+    enum Colores {Rojo, Amarillo , Azul, Blanco};
+}
+
+
+
+
---
+++ TP3/tp3Consola_7/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace tp3Consola_7
+{
+
+    public struct Auto  {
+
+        public string nombre;
+        public string descripcion;
+    }
+    public struct Persona{
+
+        public string nombre;
+        public string apellido;
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        Auto miAuto;
+        miAuto.nombre= "Fiat";
+        miAuto.descripcion="Tiene 4 puertas y es a gas.";
+        
+       Console.WriteLine($"Contenido de la Estructura Auto: {miAuto.nombre} ademas {miAuto.descripcion}");
+
+       Persona p;
+        p.nombre= "Matias";
+        p.apellido= "Bruna";
+
+        Console.WriteLine($"Contenido de la Estructura Persona; {p.nombre} {p.apellido} ");
+        
+
+
+
+
+
+        }
+    }
+}
