2f0d63030b63a153138f103b1461b342b0f54086
Matias Bruna <matias.b.dev@gmail.com>
2022-03-30 00:00:09 -0300

    cree la carpeta del ejercio 3 del tp3 y resolvi el enunciado

---
+++ TP3/tp3Consola_3/Program.cs
@@ -0,0 +1,10 @@
+﻿
+string nombreCliente, apellidoCliente ;
+Console.WriteLine("Ingrese el Nombre del Cliente:");
+nombreCliente = Console.ReadLine();
+
+Console.WriteLine("Ingrese el Apellido del Cliente:");
+apellidoCliente= Console.ReadLine();
+
+Console.WriteLine($"{nombreCliente} {apellidoCliente}");
+
