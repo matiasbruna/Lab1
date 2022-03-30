6486a5208b0f2492dc0211b4caf5fb82f9a79c32
Matias Bruna <matias.b.dev@gmail.com>
2022-03-29 22:56:33 -0300

    creo la carpeta del tp3 y sus sub carpetas para los distintos ejercicios, me pongo a trabajar en el ejercicio 1 del TP

---
+++ TP3/tp3Consola_1/Program.cs
@@ -0,0 +1,62 @@
+﻿//--A--//
+
+string var1 = "Matias";
+string var2 = "Bruna";
+
+Console.WriteLine($"Muestro variables: {var1} {var2}");
+
+//--B--//
+
+byte var3 = 0;      ///las variables byte son unicamente de de num enteros sin signo y de 0 a 255.
+byte var4 = 0;
+
+Console.WriteLine($"muestro las variables: {var3} {var4}");
+
+//--C--//
+
+byte var5 = 300;      //El 300 no esta dentro del rango del tipo de variable.
+byte var6 = 150;      //las variables byte son unicamente de de num enteros sin signo y de 0 a 255.
+
+Console.WriteLine($"muestro las variables: {var5} {var6}");
+
+//--D--//
+
+short var7 = -10;      /// variables con signo de numeros enteros hasta 16 bits.
+short var8 = -100;
+
+Console.WriteLine($"muestro las variables: {var7} {var8}");
+
+//--E--//
+
+short var9 = 40;      ///las variables byte son unicamente de de num enteros sin signo y de 0 a 255.
+short var10 = 600;
+
+Console.WriteLine($"muestro las variables: {var9} {var10}");
+
+//--F--//
+
+int var11 = -11;
+int var12 = -12;
+
+Console.WriteLine($"Muestro variables: {var11} {var12}");
+
+//--G--//
+
+int var13 = 13;
+int var14 = 14;
+
+Console.WriteLine($"Muestro variables: {var13} {var14}");
+
+//--H--//
+
+sbyte var15 = -15;  //sbyte: variables de 8 bits tipo entero y el rango es de -128 a 127.
+sbyte var16 = -16;
+
+Console.WriteLine($"Muestro variables: {var15} {var16}");
+
+//--H--//
+
+sbyte var17 = -15;  //variables de 8 bits tipo entero y el rango es de -128 a 127.
+sbyte var18 = -16;
+
+Console.WriteLine($"Muestro variables: {var15} {var16}");
