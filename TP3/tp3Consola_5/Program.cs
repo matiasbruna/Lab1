ca861c6f0aa9f69dfc762af150f1710be2911848
Matias Bruna <matias.b.dev@gmail.com>
2022-03-30 00:56:29 -0300

    creo la carpeta de dos ejercicios mas y resuelvo el ejercio numero 5 de enumeraciones

---
+++ TP3/tp3Consola_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tp3Consola_5
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        estados programa = estados.inactivo;
+
+        Console.WriteLine(programa);
+
+        }
+    }
+
+  enum estados {activo, inactivo, error};
+}
