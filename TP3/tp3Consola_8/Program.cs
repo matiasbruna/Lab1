e9a358fe3fdab7803f4eb3fa58bc61571d2710dd
Matias Bruna <matias.b.dev@gmail.com>
2022-03-30 15:45:15 -0300

    Agrego el ajercisio numero 8 y resulvo usando el cast explicito en una variable tipo long de 8 bytes dejandola de 4 bytes.

---
+++ TP3/tp3Consola_8/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace tp3Consola_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           int var1 = 384654684;
+           long var2 = 456546513843513843;
+
+           var2= (int)var2;
+
+           Console.WriteLine($" el Cast de var2 que es de tipo long y ahora es int, y se igualan sus tamaños:");
+           Console.WriteLine(var1);
+           Console.WriteLine(var2);
+        }
+    }
+}
