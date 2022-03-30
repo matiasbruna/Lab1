99f4cf4e3c744c0cd14ccf87ee147e56e4194a87
Matias Bruna <matias.b.dev@gmail.com>
2022-03-30 15:52:36 -0300

    corregi el ejercicio 8

--- TP3/tp3Consola_8/Program.cs
+++ TP3/tp3Consola_8/Program.cs
@@ -8,11 +8,12 @@
            int var1 = 384654684;
            long var2 = 456546513843513843;
 
-           var2= (int)var2;
+           var2= (int)var1;
 
            Console.WriteLine($" el Cast de var2 que es de tipo long y ahora es int, y se igualan sus tamaños:");
            Console.WriteLine(var1);
            Console.WriteLine(var2);
+           Console.ReadKey();
         }
     }
 }
