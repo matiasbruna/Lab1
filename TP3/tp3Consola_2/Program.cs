84cac671c1d553746269b0262988eabf215da727
Matias Bruna <matias.b.dev@gmail.com>
2022-03-29 23:43:32 -0300

    cree la carpeta del ejercico 2 del tp3 y resolvi el enunciado

---
+++ TP3/tp3Consola_2/Program.cs
@@ -0,0 +1,16 @@
+﻿//----D-B-C----//
+int var1 = 1;
+int var2 = 2;
+
+var1+= var1;
+var2+= var2;
+
+Console.WriteLine($"variable 1:{var1} ; varible 2: {var2}");
+
+//----D-E-F----//
+
+int var3 = 3;
+
+var3-= var3;
+
+Console.WriteLine ($"varible 3: {var3}");
