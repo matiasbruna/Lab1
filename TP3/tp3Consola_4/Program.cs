7379d1158bd46ce06b19a60d4058e96d2f4c7131
Matias Bruna <matias.b.dev@gmail.com>
2022-03-31 01:17:38 -0300

    termino el ejercio numero 4 trabajando con incrementos y decrementos.

--- TP3/tp3Consola_4/Program.cs
+++ TP3/tp3Consola_4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace tp3Consola_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 21;
+            int edad2 = 25;
+
+            edad++;
+            
+            edad2--;
+
+            Console.WriteLine($"Muestro incremento {edad} , muestro decremento {edad2}");
+            Console.ReadKey();
+        }
+    }
+}
