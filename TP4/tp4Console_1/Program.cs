15efdcd7d629197078e70a56ac32dbb292dc378c
Matias Bruna <matias.b.dev@gmail.com>
2022-04-06 00:08:58 -0300

    Realize el ejercicio numero uno de validar un numero para saber si es par o impar.

--- TP4/tp4Console_1/Program.cs
+++ TP4/tp4Console_1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Numero;
+            Console.WriteLine("Escribe un Numero Entero:");
+            Numero = int.Parse(Console.ReadLine());
+            
+            if ((Numero%2)==0)
+            {
+                Console.WriteLine("El Numero es Par");
+            }
+            else
+            { 
+                Console.WriteLine("El Numero es Impar");    
+            }
+            Console.ReadKey();           
+        
+
+
+        }
+    }
+}
