a9dc3bf1ef5bf330f88591bec444a046bdc32615
Matias Bruna <matias.b.dev@gmail.com>
2022-04-06 00:56:45 -0300

    hice el ejercio 3 del tp4, sumar dos numeros y ver si el resultado es par o impar.

---
+++ TP4/tp4Console_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Numero1, Numero2, suma ;
+
+            Console.WriteLine("Escriba un Numero: ");
+            Numero1 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Escriba un Numero: ");
+            Numero2 = int.Parse(Console.ReadLine());
+            
+            suma = Numero1 + Numero2;
+            
+            if ((suma %2) == 0)
+            {
+                Console.WriteLine($"La suma da como resultado un Numero Par = {suma}");    
+            }
+            else
+            {
+                Console.WriteLine($"La suma da como resultado un Numero Impar = {suma}");
+            }
+                
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
