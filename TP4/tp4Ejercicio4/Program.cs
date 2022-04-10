e5020ec737d21ce0c0d988e022f45efab4274830
Matias Bruna <matias.b.dev@gmail.com>
2022-04-10 20:55:23 -0300

    Cree el ejercicio numero 4 del tp4 y resolvi. se trata de buscar cuantos caracteres tiene una variable de tipo string y validar.

---
+++ TP4/tp4Ejercicio4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace tp4Ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+            int cantidad = 0 ;
+
+            Console.Clear();
+            Console.WriteLine("Ingrese la cantidad de veces que quiere ejecutar el Programa.");
+            cantidad = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cantidad; i++)
+            {
+            Console.Clear();
+            Console.WriteLine("Ingrese una Palabra:");    
+            texto = Console.ReadLine();
+            
+            if (texto.Length< 5)
+            {
+                Console.WriteLine($"La palabra: {texto}");
+                Console.WriteLine($"La cantidad de caracteres es menor que 5.");
+
+            }
+            else
+            {
+                Console.WriteLine($"La palabra: {texto}");
+            }
+            
+            Console.ReadKey();
+
+            }
+            
+        }  
+
+
+
+    }
+}
