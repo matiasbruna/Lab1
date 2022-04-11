894bba338d01ac0252fea9df019492efded8f0f4
Matias Bruna <matias.b.dev@gmail.com>
2022-04-10 23:51:45 -0300

    terminado el ejercicio7 de factorizar un numero

--- TP4/tp4Ejercicio7/Program.cs
+++ TP4/tp4Ejercicio7/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tp4Ejercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Numero = 0;
+            long Factorial = 0;
+            
+            Console.Clear();
+            Console.WriteLine("Ingrese el Numero a Factoriar:");
+            Numero = int.Parse(Console.ReadLine());
+            
+            Factorial = Numero;
+
+            for (int i = 1; i < Numero; i++)
+            {
+                 Factorial = Factorial * i;
+            }
+
+            Console.WriteLine($"El factorial del Numero {Numero} es: {Factorial}");
+        }
+    }
+}
