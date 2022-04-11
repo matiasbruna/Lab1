7563ae130c17efd49146f379d8053e3620fd67df
Matias Bruna <matias.b.dev@gmail.com>
2022-04-11 01:54:17 -0300

    se termino los ejercicios 8 y 9 del tp4 tema: numeros primos

--- TP4/tp4Ejercicio8/Program.cs
+++ TP4/tp4Ejercicio8/Program.cs
@@ -6,7 +6,35 @@ namespace tp4Ejercicio8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          int Numero = 0;
+          double Contador = 0;  
+
+          Console.Clear();
+          Console.WriteLine("Ingrese un Numero para saber si es primo");
+          Numero = int.Parse(Console.ReadLine());
+
+          for (int i = 1; i <= Numero  ; i++)
+          {
+           if (Numero % i  == 0)
+           {
+           Contador ++;    
+           }          
+          }
+          if (Numero == 1)
+          {
+            Contador = Contador +1;
+          }
+          if (Contador!= 2)
+          {
+            Console.WriteLine($"El Numero {Numero} , no es Primo");
+                                      
+          }
+          else
+          {
+            Console.WriteLine($"El numero {Numero} si es Primo.");  
+          }
+          
+
         }
     }
 }
---
+++ TP4/tp4Ejercicio9/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace tp4Ejercicio9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        int Numero = 0;
+        double Contador = 0;
+        int cantidad = 0; 
+        int primos = 0;
+        int noPrimos = 0; 
+
+        Console.Clear();
+        Console.WriteLine("Ingrasa la cantidad de Numeros para evaluar");
+        cantidad = int.Parse(Console.ReadLine());
+        
+        for (int j = 0 ;j< cantidad;j++)
+        {
+
+          Console.Clear();
+          Console.WriteLine("Ingrese un Numero: ");
+          Numero = int.Parse(Console.ReadLine());
+
+          for (int i = 1; i <= Numero  ; i++)
+          {
+           if (Numero % i  == 0)
+           {
+           Contador ++;    
+           }          
+          }
+          if (Numero == 1)
+          {
+            Contador = Contador +1;
+          }
+          if (Contador!= 2)
+          {
+            noPrimos ++;
+                                      
+          }
+          else
+          {
+            primos++;
+          }
+          Contador = 0;
+        } 
+            Console.Clear();
+          Console.WriteLine($"Se enconstraron {primos} numeros Primos y {noPrimos} no Primos.");
+
+        }
+    }
+}
