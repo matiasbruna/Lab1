ad91b02529e1a020694085cea379cb0f056435ca
Matias Bruna <matias.b.dev@gmail.com>
2022-04-10 23:54:05 -0300

    Se creo el ejercicio 8 del tp4 a trabjar.

---
+++ TP4/tp4Ejercicio8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tp4Ejercicio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
