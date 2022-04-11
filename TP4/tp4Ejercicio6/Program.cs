dd77472db1bb1009bff5f2f8be4814566af2bb7f
Matias Bruna <matias.b.dev@gmail.com>
2022-04-10 22:53:51 -0300

    Se realizo el ejercicio6 se probo y todo funciona perfecto

--- TP4/tp4Ejercicio6/Program.cs
+++ TP4/tp4Ejercicio6/Program.cs
@@ -6,7 +6,25 @@ namespace tp4Ejercicio6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Double Suma = 0 ;
+            
+            for (int i = 0; i < 15; i++)
+            {
+            Console.Clear();    
+            Console.WriteLine("Ingrese Un Numero "); 
+            Suma = Suma + int.Parse(Console.ReadLine());   
+            }
+            
+            if (Suma == 0)
+            {
+                Console.WriteLine("NO se puede Hacer el Promedio de Cero (0)");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine($"El promedio es: {Suma /15}");
+            }
+        
         }
     }
 }
