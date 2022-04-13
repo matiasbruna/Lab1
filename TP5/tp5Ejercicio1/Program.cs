3bbe580c97b2496072a440726ebd944f9af471c5
Matias Bruna <matias.b.dev@gmail.com>
2022-04-12 21:40:54 -0300

    Termine el ejercicio 1 del tp4

---
+++ TP5/tp5Ejercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace tp5Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int Numero = 0;
+                
+
+                Console.Clear();
+
+                Console.WriteLine("Ingrese un numero:");
+                Numero = int.Parse(Console.ReadLine());
+
+            }
+            catch (FormatException)
+            {       
+                Console.WriteLine("Cuidado!!!, Ingrese un Numero.");
+                
+            }
+        }
+    }
+}
