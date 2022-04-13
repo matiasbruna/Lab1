986db461ecf0098987ed47fe40459a86f0d1b0b2
Matias Bruna <matias.b.dev@gmail.com>
2022-04-12 23:00:12 -0300

    Se termino todo el TP5

---
+++ TP5/tp5Ejercicio3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace tp5Ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            int Numero = int.MaxValue;
+
+            Console.Clear();  
+            
+            Console.WriteLine($" {Numero} es: ");
+
+            Numero++;
+
+            Console.WriteLine($" {Numero} es: ");
+            
+            
+
+            }
+            
+            catch (OverflowException ex)
+            {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Varible Desbordada.");
+            }
+        }
+
+    }
+}
---
+++ TP5/tp5Ejercicio4/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+
+namespace tp5Ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           try
+           {
+               int Valor1 = 0;
+               int Valor2 = 0;
+
+                Console.Clear();
+                
+                Console.WriteLine("Ingrese un Numero:");
+                Valor1 = int.Parse(Console.ReadLine());
+
+                
+                Console.WriteLine("Ingrese un Numero:");
+                Valor2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"La division es: {Valor1 / Valor2}");
+
+
+           }
+           catch (DivideByZeroException ex)
+           {
+               Console.WriteLine(ex.Message);
+                Console.WriteLine("Cuidado!!!, estas dividiendo por Cero (0)");
+
+           }
+            catch (FormatException ex)
+           {
+               Console.WriteLine(ex.Message);
+                Console.WriteLine("Cuidado!!!, Formato Incorrecto");
+           }
+                      catch (Exception ex)
+           {
+               Console.WriteLine(ex.Message);
+                Console.WriteLine("Mensaje de exepcion.");
+           }
+            finally
+            {
+                Console.WriteLine("Este Bloque de codigo se ejecuta seimpre.");
+
+
+            }
+
+
+        }
+    }
+}
