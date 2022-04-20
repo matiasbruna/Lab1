6c3dfa9fad3c247608c9d0488cb52944ea5b8b23
Matias Bruna <matias.b.dev@gmail.com>
2022-04-19 22:49:39 -0300

    Se creo el propuesto2 pero sin resolver

---
+++ Propuestos/Propuestos2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Propuestos2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
