d763504303854ed72eff419a7758ea99ac8435e3
Matias Bruna <matias.b.dev@gmail.com>
2022-05-18 14:54:14 -0300

    Se resolvio el ejercicio numero 3 metodo que llama a otro metodo.

---
+++ TP6/Ejercicio3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 0;
+            double num2 = 0;
+
+            Console.WriteLine("Ingrese un numero:");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese un numero:");
+            num2 = double.Parse(Console.ReadLine());
+
+            metodoLlamador(num1, num2);
+        }
+        static void metodoLlamador(double n1, double n2)
+        {
+            
+            double Resultado = calculo (n1, n2);    // llama el metodo para hacer la Mult.
+            
+            Console.WriteLine(n1+n2);
+            Console.WriteLine(Resultado);
+
+
+        } 
+        static double calculo (double a, double b)
+        {
+            return a * b; 
+        }
+    }
+}
