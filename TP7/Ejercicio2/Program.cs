35503c534cc6b8f2646960ebe611f1301b1b65e2
Matias Bruna <matias.b.dev@gmail.com>
2022-05-24 23:31:35 -0300

    termine el ej3

--- TP7/Ejercicio2/Program.cs
+++ TP7/Ejercicio2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double [] Numeros = new double [5];
+            double Suma = 0;
+            for (int i = 0; i < Numeros.Length; i++)
+            {
+                Console.Clear();
+                Console.WriteLine ("Ingrese un Numero:");
+                Numeros[i]= double.Parse(Console.ReadLine());
+            }
+            
+            for (int i = 0; i < Numeros.Length; i++)
+            {
+                Suma = Suma + Numeros[i];
+            }   
+           
+            Console.Clear();
+            Console.WriteLine($"El promedio de los numeros Ingresados es: {Suma/ Numeros.Length}");
+        }
+    }
+}
