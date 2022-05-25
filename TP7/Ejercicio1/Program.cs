b251da8bb23e57fac21f50b658a9f2ca81471cbb
Matias Bruna <matias.b.dev@gmail.com>
2022-05-24 23:10:05 -0300

    Termine el ejercicio1 y cree el ej2.

--- TP7/Ejercicio1/Program.cs
+++ TP7/Ejercicio1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Ejercicio1   ///arrays, vectores, tablas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] nombre  = new String [] {"Matias","Guillermo","Ana Laura","Paola","Jose" };
+
+            for (int i = 0; i < nombre.Length; i++)  
+            {
+                Console.WriteLine(nombre[i]);
+            }
+            
+        }
+    }
+}
