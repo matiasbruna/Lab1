b9d0b0628cb3111d52dc208eb8d17242b31379d2
Matias Bruna <matias.b.dev@gmail.com>
2022-05-25 01:58:19 -0300

    termine el eje5 use el string.Format pata pormar filas y columnas

---
+++ TP7/Ejercicio5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ejercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] Nombres = new string[5];
+            double[] Nota = new double [5];
+
+            
+            for (int i = 0; i < Nombres.Length; i++)
+            {
+                Console.Clear();
+                Console.WriteLine("Ingresar Nombre: ");
+                Nombres [i] = Console.ReadLine();
+                Console.WriteLine("Ingresar Nota: ");
+                Nota[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.Clear();
+
+            for (int i = 0; i < Nombres.Length; i++)
+            {    
+                Console.WriteLine( string.Format("{0,-10} {1,-10}",Nombres[i], Nota[i]));
+            } 
+        }
+
+
+    }
+}
