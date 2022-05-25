795a35cb4f20697aebbf68630fb1386ca6c1881d
Matias Bruna <matias.b.dev@gmail.com>
2022-05-25 00:44:09 -0300

    Termine el eje4

--- TP7/Ejercicio4/Program.cs
+++ TP7/Ejercicio4/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Nombres = new string[]{"Mario", "Ana","Jose","Eduardo","Norberto"};
+
+           Console.WriteLine("Muestra el vector Original: ");
+           Mostrar(Nombres);
+
+            Console.WriteLine("Muestra el vector ordenado Alfabeticamnte: ");
+            Array.Sort(Nombres);
+            Mostrar(Nombres);
+
+        }
+
+        static void Mostrar (string[] arr)
+        {
+            foreach (var Nombres in arr)
+            {
+                Console.Write($"{Nombres}; ");
+            }   
+            Console.WriteLine("\n");
+        } 
+    }
+}
