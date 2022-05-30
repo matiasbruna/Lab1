cda0167db3121188e3813cefae1339974f95737f
Matias Bruna <matias.b.dev@gmail.com>
2022-05-29 22:30:17 -0300

    Termine el ejercicio2 del propuestos 3

--- Propuestos/Propuestos3.2/Program.cs
+++ Propuestos/Propuestos3.2/Program.cs
@@ -6,7 +6,31 @@ namespace Propuestos3_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int Dia = 1;
+            int Mes = 1;
+            Console.Clear();
+            Console.WriteLine("Ingrese una Fecha para evaluar: ");
+            Console.Write("Dia: ");
+            Dia = int.Parse(Console.ReadLine());
+            Console.Write("Mes: ");
+            Mes = int.Parse(Console.ReadLine());
+
+            if (Dia >= 21 && Mes == 12|| Mes== 01 || Mes == 02 || Mes == 03 && Dia <=20)
+            {
+                System.Console.WriteLine($"La fecha:{Dia}/{Mes}, pertenece a la estacion, Verano.");
+            }
+            if (Dia >= 21 && Mes == 03|| Mes== 04 || Mes == 05 || Mes == 06 && Dia <=20)
+            {
+                System.Console.WriteLine($"La fecha:{Dia}/{Mes}, pertenece a la estacion, Otoño.");
+            }
+            if (Dia >= 21 && Mes == 06|| Mes== 07 || Mes == 08 || Mes == 09 && Dia <=20)
+            {
+                System.Console.WriteLine($"La fecha:{Dia}/{Mes}, pertenece a la estacion, Invierno.");
+            }
+            if (Dia >= 21 && Mes == 09|| Mes== 10 || Mes == 11 || Mes == 12 && Dia <=20)
+            {
+                System.Console.WriteLine($" La fecha:{Dia}/{Mes}, pertenece a la estacion, Primavera.");
+            }
         }
     }
 }
