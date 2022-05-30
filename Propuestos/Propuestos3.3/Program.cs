b2e2281a5a0b15e2c21eaef82a9f52d26307b735
Matias Bruna <matias.b.dev@gmail.com>
2022-05-29 22:37:57 -0300

    se creo la solucion del ejercicio3 del propuestos3

---
+++ Propuestos/Propuestos3.3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Propuestos3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+         
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
