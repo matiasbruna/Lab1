c1ae92fe6c52965c491cf5aa4406b487593b2b78
Matias Bruna <matias.b.dev@gmail.com>
2022-05-31 20:38:55 -0300

    resolvi el ejercicio6  del tp7

--- TP7/Ejercicio6/Program.cs
+++ TP7/Ejercicio6/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Ejercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string [,] matriz = {{"Fiat","Ferrari","Ford"},{"Chebrolet","Audi","Citroen"}};
+
+
+            MostrarMatriz(matriz);
+            
+        }
+        static void MostrarMatriz (string [,] M)
+        {
+
+            foreach (var item in M)
+            {
+                System.Console.WriteLine(item);
+            }
+
+        }
+    }
+}
