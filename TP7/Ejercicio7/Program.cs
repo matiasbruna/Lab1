995f8cee56cc49c39a20c53622f722675d45b115
Matias Bruna <matias.b.dev@gmail.com>
2022-05-31 21:52:17 -0300

    Termine el ejercicio7 del tp7 suma de filas de matrices cargadas por pantalla.

--- TP7/Ejercicio7/Program.cs
+++ TP7/Ejercicio7/Program.cs
@@ -6,7 +6,52 @@ namespace Ejercicio7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+        int [,] matriz = new int [5,5];
+
+        CargarMatriz (matriz);
+        MostrarMatriz(matriz);
+
         }
+
+        static void CargarMatriz(int[,] M)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Clear();
+                System.Console.WriteLine($"Ingrese los valores de la Fila {i+1}:");  
+                
+                for (int j = 0; j < 5; j++)
+                {
+                    System.Console.WriteLine($"Valor {j+1}:");
+                    M[i,j] = int.Parse(Console.ReadLine());
+
+                }
+
+            }
+          
+        }
+
+        static void MostrarMatriz(int[,] M)
+        {
+            int suma ;
+            Console.Clear();
+
+            for (int i = 0; i < 5; i++)
+            {
+               
+                System.Console.WriteLine($"Muestro los valores de la Fila {i+1} y resuelvo:");  
+                suma = 0;
+                for (int j = 0; j < 5; j++)
+                {
+                    System.Console.Write($"{M[i,j]}, ");
+                    suma = suma + M[i,j];
+
+                }
+                Console.Write ($" = {suma}.\n");
+
+            }
+        }
+
     }
 }
