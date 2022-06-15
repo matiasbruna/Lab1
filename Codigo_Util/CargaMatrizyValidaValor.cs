7efb442da240482ce9e17e5eefcf219a624ba8c1
Matias Bruna <matias.b.dev@gmail.com>
2022-06-14 23:48:18 -0300

    validacion de ingreso de datos y cree archivo con codigo para reutilizar

---
+++ Codigo_Util/CargaMatrizyValidaValor.cs
@@ -0,0 +1,40 @@
+static double[,] CargarMatriz(int F, int C)     
+{
+    double[,] M = new double[F,C];
+    System.Console.WriteLine("Ingrese los valores para cargar la matriz:");
+    for (int i = 0; i < M.GetLength(0); i++)
+    {
+        for (int j = 0; j < M.GetLength(1); j++)
+        {
+            
+            M[i,j] = ValidaValor();
+        }
+    }
+
+    return M; 
+}
+
+static double ValidaValor(int i, int j)     
+{
+    
+    Boolean Condicion = true;
+    double valor = 0;        
+        
+    while (Condicion)
+    {
+        
+        System.Console.Write($"Nota[{i},{j}] : ");
+        valor = double.Parse(Console.ReadLine());
+        
+        if (valor<= 10 & valor >= 1)
+        {
+        Condicion = false;
+        }
+        else
+        {
+        System.Console.WriteLine("Debe ingresar un numero de 1 al 10");
+        }
+    }
+            
+    return valor ;
+}
--- Propuestos/Propuestos4.3/Program.cs
+++ Propuestos/Propuestos4.3/Program.cs
@@ -59,16 +59,40 @@ namespace Propuestos4_3
         {
             double[,] M = new double[F,C];
             System.Console.WriteLine("Ingrese los valores para cargar la matriz:");
+            
             for (int i = 0; i < M.GetLength(0); i++)
             {
                 for (int j = 0; j < M.GetLength(1); j++)
-                {
-                    System.Console.Write($"Fila {i}, valor {j}: ");
-                    M[i,j] = double.Parse(Console.ReadLine());
+                {                   
+                    M[i,j] = ValidaValor(i,j);
                 }
             }
-
             return M; 
+        }
+
+        static double ValidaValor(int i, int j)     
+        {
+            
+            Boolean Condicion = true;
+            double valor = 0;        
+                
+            while (Condicion)
+            {
+              
+             System.Console.Write($"Nota[{i}{j}] : ");
+             valor = double.Parse(Console.ReadLine());
+                
+             if (valor<= 10 & valor >= 1)
+             {
+             Condicion = false;
+             }
+             else
+             {
+              System.Console.WriteLine("Debe ingresar un numero de 1 al 10");
+             }
+            }
+                 
+            return valor ;
         }
     }
 }
