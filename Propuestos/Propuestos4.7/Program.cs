b04b4e201ebad62ae6908eff6fff02285a9f940e
Matias Bruna <matias.b.dev@gmail.com>
2022-06-20 18:45:20 -0300

    Cree y termine el ejercicio 7 de propuestos 4

---
+++ Propuestos/Propuestos4.7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Propuestos4_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] Nombres = {"Matias", "Juan", "Gonzalo", "Maria"};
+
+            foreach (var item in Nombres)
+            {
+                System.Console.WriteLine(item);
+            }
+
+        }
+    }
+}
