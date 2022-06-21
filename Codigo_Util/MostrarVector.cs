f7151859632646fe322b124494cd6d3043f9bffe
Matias Bruna <matias.b.dev@gmail.com>
2022-06-20 23:46:56 -0300

    Termine el Eje6 del Propuestos 5 y agrege mas codigo Util a la Carpeta.

---
+++ Codigo_Util/MostrarVector.cs
@@ -0,0 +1,9 @@
+static void MostrarVector(double[]V)
+{
+    for (int i = 0; i < V.Length; i++)
+    {
+        Console.Write($"{V[i]} ");
+    }
+
+    Console.Write("\n");
+}
--- Propuestos/Propuestos5.6/Program.cs
+++ Propuestos/Propuestos5.6/Program.cs
@@ -6,8 +6,47 @@ namespace Propuestos5_6
     {
         static void Main(string[] args)
         {
-
+            double [,] Matriz = {
+                {30, 54, 25, -98 },
+                {-18, 35, 2, 4,},
+                {60, 125, 8, 39 },
+                {30, 14, 36, 78}
+            };
+            Console.Clear();
+            double[] Vector =  VectorDiagonal(Matriz);
             
+            System.Console.Write("Vector: ");
+            MostrarVector(Vector);
+            
+            Array.Sort(Vector);
+            System.Console.Write("Vector ordenado: ");
+            MostrarVector(Vector);
+    
+        }
+        static void MostrarVector(double[]V)
+        {
+            for (int i = 0; i < V.Length; i++)
+            {
+                Console.Write($"{V[i]} ");
+            }
+
+            Console.Write("\n");
+        }
+        static double[] VectorDiagonal (double[,] M)
+        {
+            double[] V = new double[M.GetLength(0)];
+
+            for (int i = 0; i < M.GetLength(0); i++)
+            {   
+                for (int j = 0; j < M.GetLength(1); j++)
+                {
+                     if (j == (M.GetLength(1) - 1) - i) //diagonal Invertida
+                    {
+                       V[i] = M[i,j];          
+                    }
+                }
+            }
+            return V;
         }
     }
 }
